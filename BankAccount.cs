c6da40b2a75a4c63f99cdfe1323be074bebd841e
Mohamed TELLI <tellimohamed91@gmail.com>
2025-03-20 18:21:31 +0100

    first commit

---
+++ BankAccount.cs
@@ -0,0 +1,66 @@
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.InteropServices;
+using Microsoft.VisualBasic;
+
+namespace Classes;
+
+public class BankAccount
+{
+  private static int s_accountNumberSeed = 1234567890;
+    public string Number { get; }
+    public string Owner { get; set; }
+    public decimal Balance { 
+      get{
+        decimal balance = 0;
+        foreach(var items in _allTransactions)
+        {
+          balance += items.Amount;
+        }
+        return balance;
+      } 
+    }
+
+    public BankAccount(string name, decimal initialBalance)
+    {
+      this.Number = s_accountNumberSeed.ToString();
+      this.Owner = name;
+      MakeDeposit(initialBalance, DateTime.Now, "Initial balance");
+      s_accountNumberSeed++;
+    }
+    private List<Transaction> _allTransactions = new List<Transaction>();
+    public void MakeDeposit(decimal amount, DateTime date, string note)
+    {
+      if(amount <= 0)
+      {
+        throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be positive");
+      }
+      var deposit = new Transaction(amount, date, note);
+      _allTransactions.Add(deposit);
+    }
+
+    public void MakeWithdrawal(decimal amount, DateTime date, string note)
+    {
+      if (amount <= 0)
+      {
+        throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be positive");
+      }
+      if (Balance - amount < 0)
+      {
+        throw new InvalidOperationException("Not sufficient fund for this withdrawal");
+      }
+      var withdrawal = new Transaction(-amount, date, note);
+      _allTransactions.Add(withdrawal);
+    }
+
+    public string GetAccountHistory(){
+      var report = new System.Text.StringBuilder();
+      
+      decimal balance = 0;
+      report.AppendLine("Date\t\tAmount\tBalance\tNote");
+      foreach(var item in _allTransactions){
+        balance += item.Amount;
+        report.AppendLine($"{item.Date.ToShortDateString()}\t{item.Amount}\t{balance}\t{item.Notes}");
+      }
+      return report.ToString();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Runtime.CompilerServices;
+using Classes;
+
+var account = new BankAccount("Telli", 1000);
+Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
+account.MakeWithdrawal(500, DateTime.Now, "Rent payment");
+Console.WriteLine(account.Balance);
+account.MakeDeposit(100, DateTime.Now, "Friend paid me back");
+Console.WriteLine(account.Balance);
+
+Console.WriteLine(account.GetAccountHistory());
+
+// Test for amount of withdrawal higher than balance
+try{
+  account.MakeWithdrawal(1000, DateTime.Now, "Invalid Withdrawal");
+}
+catch(InvalidOperationException e){
+  Console.WriteLine("Withdrawal can't be higher than balance");
+  Console.WriteLine(e.ToString());
+  return;
+}
+
+// Test for negative withdrawal amount
+try{
+  account.MakeWithdrawal(-500, DateTime.Now, "Invalid Withdrawal");
+}
+catch (ArgumentOutOfRangeException e) {
+  Console.WriteLine("Withdrawal amount must be positive");
+  Console.WriteLine(e.ToString());
+  return;
+}
+
+
+// Test for negative deposit amount
+try{
+  account.MakeDeposit(-51, DateTime.Now, "Invalid deposit");
+}
+catch(ArgumentOutOfRangeException e)
+{
+  Console.WriteLine("Deposit amount must be positive");
+  Console.WriteLine(e.ToString());
+  return;
+}
+
+// Test for negative initial balance when creating an account
+BankAccount invalidAccount;
+try {
+  invalidAccount = new BankAccount("invalid", -500);
+}
+catch (ArgumentOutOfRangeException e) {
+  Console.WriteLine("Exception caught creating account with negative initial balance");
+  Console.WriteLine(e.ToString());
+  return;
+}
+
